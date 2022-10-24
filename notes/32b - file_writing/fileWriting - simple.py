3e634a4228f78d477daf92b1946b6d49ac9b321a
Chris Brooks-Prenger <brooksprench@hdsb.ca>
2022-10-24 14:27:31 -0400

    Update fileWriting - simple.py
    Update comments to further clarify for students

--- notes/32b - file_writing/fileWriting - simple.py
+++ notes/32b - file_writing/fileWriting - simple.py
@@ -8,8 +8,10 @@
 # Updated:     21-Oct-2021
 #-----------------------------------------------------------------------------
 
-string = "This will be written to a file"
+string = "This will be written to a file\r\n"
 
+# Using a 'w' erases everything in the file that was already there
+# Using a 'a' adds the content to the end of the file.
 file = open('output.txt', 'a')
 file.write(string)
 file.close()
